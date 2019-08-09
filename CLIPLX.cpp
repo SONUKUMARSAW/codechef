6fe0da7f3582a6ec9fe5ace9681efc52eef4d05d
SONUKUMARSAW <sonukumarsaw66@gmail.com>
2019-08-09 22:55:18 +0530

    CodeChef beginner DAY5

---
+++ CLIPLX.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef long double ld;
+
+#define FOR(a,b) for(auto i=a;i<b;i++)
+#define FAIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define test_cases int T;cin>>T;while(T--)
+
+
+int main(){
+    FAIO
+
+    int X,Y,temp;
+    test_cases{
+        cin>>X>>Y;
+        cout<<max(0,X-Y)<<endl;
+    }
+
+    return 0;
+}
+
+
+/*
+You can always say me thanks
+Autthor: SONUKUMARSAW
+email: sonukumarsaw66@gmail.com
+*/
